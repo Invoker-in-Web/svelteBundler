8f4e843c5b818deae7b6d30a70fbe1b2f66a6495
Игорь Манченко <i.manchenko@itfbgroup.ru>
2020-01-21 16:40:42 +0300

    9) add some functional to buttons

--- src/Button.svelte
+++ src/Button.svelte
@@ -1,5 +1,25 @@
 <script>
+  const props = {
+    id: 1251243,
+    phone: "8-999-555-44-22",
+    address: "Earth",
+    street: "Broken Lamps",
+    home: 666
+  };
+  function firstButtonHandle() {
+    const dummyText = "FirstText";
+    document.getElementsByClassName("customTextarea")[0].append(dummyText);
+    document.getElementsByClassName("customTextarea").id = props.id;
+    document.getElementsByClassName("customTextarea").phone = props.phone;
+    document.getElementsByClassName("customTextarea").address = props.address;
+  }
 
+  function secondButtonHandle() {
+    const dummyText = "SecondText";
+    document.getElementsByClassName("customP")[0].append(dummyText);
+    document.getElementsByClassName("customP").street = props.street;
+    document.getElementsByClassName("customP").home = props.home;
+  }
 </script>
 
 <style>
@@ -16,11 +36,13 @@
 
   .firstButton {
     display: flex;
+    justify-content: center;
     color: red;
   }
 
   .secondButton {
     color: green;
+    justify-content: center;
   }
 
   .mainUL {
@@ -29,16 +51,50 @@
     }
     .second {
       color: $aquaColor;
-      @mixin typicalBorder;
     }
     .third {
       color: $brownColor;
     }
   }
+
+  .buttons {
+    display: flex;
+    justify-content: space-evenly;
+  }
+
+  .wrapper {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: flex-start;
+  }
+  .firstField,
+  .secondField {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    max-width: 400px;
+  }
+
+  .customP {
+    @mixin typicalBorder;
+  }
 </style>
 
-<button class="firstButton">Первая Кнопка</button>
-<button class="secondButton">Вторая Кнопка</button>
+<div class="wrapper">
+  <div class="firstField">
+    <button class="firstButton" on:click={firstButtonHandle}>
+      Первая Кнопка
+    </button>
+    <textarea class="customTextarea" cols="30" rows="10" />
+  </div>
+  <div class="secondField">
+    <button class="secondButton" on:click={secondButtonHandle}>
+      Вторая Кнопка
+    </button>
+    <p class="customP" />
+  </div>
+</div>
+
 <ul class="mainUL">
   zxczxc
   <li class="first">какой-то текст</li>
